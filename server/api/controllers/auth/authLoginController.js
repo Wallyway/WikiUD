82f843c3772eb063b9bcbc6725fa4322f921f5cc
Sebastian Wallis <j.sebastian.aw@gmail.com>
2025-02-17 21:06:12 -0500

    Implement login functionality: add user authentication with bcrypt and return user data upon successful login

--- server/api/controllers/auth/authLoginController.js
+++ server/api/controllers/auth/authLoginController.js
@@ -1,1 +1,28 @@
-export const login = (req, res) => res.send("Login");
+import User from "../../models/userModel.js";
+import bcrypt from "bcrypt";
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
